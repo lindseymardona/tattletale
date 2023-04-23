9d971371a0eb11141a7aa15ace7864d4d0e205eb
lindseymardona <lindseymardona@gmail.com>
2023-04-23 03:57:48 -0700

    feat: logo embed top of screen

--- ionicTest/src/pages/Tab1.tsx
+++ ionicTest/src/pages/Tab1.tsx
@@ -3,18 +3,31 @@ import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import './fonts.css';
+import logo from '../../tattletale.png';
 
 const Tab1: React.FC = () => {
-  return (
+  return (  
     <IonPage>
       {/* <IonHeader>
         <IonToolbar>
           <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader> */}
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <img src={logo} alt="My Logo" style={{ height: '100px', width: 'auto' }} />
+    </div>
       <IonContent className="ion-padding">
-        <h2>hi (name),</h2>
-        <h5>you have (num_classes) classes today.</h5>
+        {/* <h2>hi (name),</h2>
+        <h5>you have (num_classes) classes today.</h5> */}
+        <h2>hi <b>stephen</b>,</h2>
+        <h5>you have <b>3</b> classes today.</h5>
+
+      <IonCard><div id="card1">
+        <IonCardHeader>
+          <IonCardSubtitle><div id="current">no ongoing classes!</div></IonCardSubtitle>
+          <br></br><br></br>
+        </IonCardHeader>
+      </div></IonCard>
         
       <IonCard><div id="card1">
         <IonCardHeader>
@@ -23,11 +36,8 @@ const Tab1: React.FC = () => {
           <IonCardSubtitle><div id="cardtext"><i>until</i></div></IonCardSubtitle>
           <IonCardSubtitle><div id="classcode">COM SCI M146</div></IonCardSubtitle>
         </IonCardHeader>
-
-        <IonCardContent>
-          Here's a small text description for the card content. Nothing more, nothing less.
-        </IonCardContent>
       </div></IonCard>
+
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Home</IonTitle>
