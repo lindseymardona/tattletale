da5d72828ab478902e726244fcee1cd32b4d5099
Stephen Hwang <jhwang0703@gmail.com>
2023-04-23 04:24:38 -0700

    feat: manual merge fix

--- ionicTest/src/pages/Tab1.tsx
+++ ionicTest/src/pages/Tab1.tsx
@@ -1,21 +1,29 @@
-import { IonGrid, IonRow, IonCol } from '@ionic/react';
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import {IonGrid, IonRow, IonCol, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import './fonts.css';
+import logo from '../../tattletale.png';
 
 const Tab1: React.FC = () => {
-  return (
+  return (  
     <IonPage>
-      {/* <IonHeader>
-        <IonToolbar>
-          <IonTitle>Home</IonTitle>
-        </IonToolbar>
-      </IonHeader> */}
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <img src={logo} alt="My Logo" style={{ height: '100px', width: 'auto' }} />
+      </div>
+
       <IonContent className="ion-padding">
-        <h2>hi (name),</h2>
-        <h5>you have (num_classes) classes today.</h5>
+        {/* <h2>hi (name),</h2>
+        <h5>you have (num_classes) classes today.</h5> */}
+        <h2>hi <b>stephen</b>,</h2>
+        <h5>you have <b>3</b> classes today.</h5>
+
+      <IonCard id = "card1">
+        <IonCardHeader>
+          <IonCardSubtitle><div id="current">no ongoing classes!</div></IonCardSubtitle>
+          <br></br><br></br>
+        </IonCardHeader>
+      </IonCard>
         
       <IonCard id = "card1">
 
@@ -41,26 +49,6 @@ const Tab1: React.FC = () => {
 
 
 
-
-          {/* <IonCardSubtitle><div id="cardtext"><i>coming up</i></div></IonCardSubtitle>
-          <IonCardSubtitle><div id="timeleft">00:54:38</div></IonCardSubtitle> */}
-          {/* <IonCardSubtitle><div id="cardtext"><i>until</i></div></IonCardSubtitle>
-          <IonCardSubtitle><div id="classcode">COM SCI M146</div></IonCardSubtitle>  */}
-{/* 
-          <IonGrid>
-            <IonRow>
-              <IonCol size="1">
-                <div style={{display: 'flex', justifyContent: 'left', alignItems: 'flex-end', height: '100%'}}>
-                  <IonCardSubtitle><div id="cardtext"><i>until</i></div></IonCardSubtitle>
-                </div>
-              </IonCol>
-              <IonCol size="10">
-                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'flex-end', height: '100%'}}>
-                  <IonCardSubtitle><div id="classcode">COM SCI M146</div></IonCardSubtitle>
-                </div>
-              </IonCol>
-            </IonRow>
-          </IonGrid> */}
 
         </IonCardHeader>
       </IonCard>
