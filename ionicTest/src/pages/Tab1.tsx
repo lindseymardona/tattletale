f4a299fdc03afa3d99a2b5a606f90f52ccaf4232
Stephen Hwang <jhwang0703@gmail.com>
2023-04-22 22:27:26 -0700

    Tab1 edit

--- ionicTest/src/pages/Tab1.tsx
+++ ionicTest/src/pages/Tab1.tsx
@@ -1,16 +1,30 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+      {/* <IonHeader>
         <IonToolbar>
           <IonTitle>Home</IonTitle>
         </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
+      </IonHeader> */}
+      <IonContent className="ion-padding" >
+        <h2>hi (name),</h2>
+        <h5>you have NUM_CLASSES classes today.</h5>
+        
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Here's a small text description for the card content. Nothing more, nothing less.
+        </IonCardContent>
+      </IonCard>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Home</IonTitle>
