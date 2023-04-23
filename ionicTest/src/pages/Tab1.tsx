c4780579fbf088a172cdc29f76cbba94bfaec884
lindseymardona <lindseymardona@gmail.com>
2023-04-23 04:05:54 -0700

    merge

--- ionicTest/src/pages/Tab1.tsx
+++ ionicTest/src/pages/Tab1.tsx
@@ -1,5 +1,4 @@
-import { IonGrid, IonRow, IonCol } from '@ionic/react';
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
@@ -29,6 +28,46 @@ const Tab1: React.FC = () => {
           <br></br><br></br>
         </IonCardHeader>
       </div></IonCard>
+        
+      <IonCard><div id="card1">
+        <IonCardHeader>
+          <IonCardSubtitle><div id="cardtext"><i>coming up</i></div></IonCardSubtitle>
+          <IonCardSubtitle><div id="timeleft">00:54:38</div></IonCardSubtitle>
+          <IonCardSubtitle><div id="cardtext"><i>until</i></div></IonCardSubtitle>
+          <IonCardSubtitle><div id="classcode">COM SCI M146</div></IonCardSubtitle>
+        </IonCardHeader>
+      </div></IonCard>
+
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Home</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        {/* <ExploreContainer name="Home" /> */}
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab1;
+import { IonGrid, IonRow, IonCol } from '@ionic/react';
+import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import React from 'react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab1.css';
+import './fonts.css';
+
+const Tab1: React.FC = () => {
+  return (
+    <IonPage>
+      {/* <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader> */}
+      <IonContent className="ion-padding">
+        <h2>hi (name),</h2>
+        <h5>you have (num_classes) classes today.</h5>
         
       <IonCard id = "card1">
 
